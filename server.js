2c5c0a0aa00824b0b0944409f7d46839d9762dfa
Jan Maršíček <janmarsicek@janmarsicek.cz>
2018-07-01 15:11:24 +0200

    Log request parameters and connect to the db

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
@@ -6,7 +8,16 @@ const app = express();
 
 const port = 8000;
 
-require("./app/router")(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(err);
+
+  // Make sure you add the database name and not the collection name (for MongoDB v3.0+)
+  db = database.db(process.env.DB_NAME)
+  require('./app/router')(app, db);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
 });
