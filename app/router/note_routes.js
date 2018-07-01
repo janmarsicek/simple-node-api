e03f586a24588b8053ad6403ea9e040cd85d3acb
Jan Maršíček <janmarsicek@janmarsicek.cz>
2018-07-01 15:23:22 +0200

    Save data to DB

--- app/router/note_routes.js
+++ app/router/note_routes.js
@@ -1,6 +1,11 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
-    console.log(req.body);
-    res.send("Hello");
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+        if (err) {
+            return res.send({'error': 'An error has occurred' });
+        }
+        return res.send(result.ops[0]);
+    })
   });
 };
